ddd730c3246d20b905f76ea12762b4656cb01de9
TrucNguyen1410 <truc141004@gmail.com>
2025-11-30 01:25:55 +0700

    sửa lỗi mất trang quên mk

--- client_art_shop/src/App.js
+++ client_art_shop/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify'; 
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'; 
 
+// --- IMPORT CÁC TRANG ---
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
@@ -14,7 +15,11 @@ import CulturePage from './pages/CulturePage';
 import CheckoutPage from './pages/CheckoutPage'; 
 import OrderHistoryPage from './pages/OrderHistoryPage';
 import ProfilePage from './pages/ProfilePage';
-import AboutPage from './pages/AboutPage'; // <--- IMPORT TRANG MỚI
+import AboutPage from './pages/AboutPage'; 
+
+// --- QUAN TRỌNG: IMPORT 2 TRANG QUÊN MẬT KHẨU ---
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
 function App() {
   const [cart, setCart] = useState(() => {
@@ -35,7 +40,6 @@ function App() {
     } else {
       setCart([...cart, { ...product, qty: 1 }]);
     }
-    
     toast.success(`Đã thêm "${product.name}" vào giỏ!`, {
         position: "top-right",
         autoClose: 2000,
@@ -58,21 +62,26 @@ function App() {
 
       <div className="container-fluid p-0">
         <Routes>
+          {/* Trang Public */}
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage addToCart={addToCart} />} />
           <Route path="/culture" element={<CulturePage />} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/product/:id" element={<ProductPage addToCart={addToCart} />} />
           <Route path="/cart" element={<CartPage cart={cart} removeFromCart={removeFromCart} />} />
           
+          {/* Xác thực (Auth) */}
           <Route path="/login" element={<LoginPage setUser={setUser} />} />
           <Route path="/register" element={<RegisterPage setUser={setUser} />} />
           
+          {/* --- QUAN TRỌNG: ROUTES QUÊN MẬT KHẨU --- */}
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          
+          {/* Trang User (Cần đăng nhập) */}
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/checkout" element={<CheckoutPage cart={cart} setCart={setCart} />} />
           <Route path="/orders" element={<OrderHistoryPage />} />
-          
-          {/* Route Về Chúng Tôi */}
-          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
     </Router>
